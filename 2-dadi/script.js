37478983d818e930d780de5ec9adf013e53e72a9
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-03 17:31:57 +0100

    feat: dadi

--- 2-dadi/script.js
+++ 2-dadi/script.js
@@ -3,3 +3,26 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
+
+// |Chiedere| all'utente di avviare la sfida lanciando il suo dado a 6 facce
+// |Generare| un numero randomico tra 1 e 6 per il giocatore
+
+const userRoll = Math.floor(Math.random() * 6) +1
+console.log("lancio giocatore", userRoll)
+
+
+// |Generare| un numero randomico tra 1 e 6 per il computer
+
+const cpuRoll = Math.floor(Math.random() * 6) +1
+console.log("lancio cpu", cpuRoll)
+
+
+// |Confrontare| il risultato dei dadi
+//      |Comunicare| il vincitore (chi pesca il numero più alto)
+if (userRoll > cpuRoll) {
+    alert(`Tu:${userRoll} vs CPU:${cpuRoll}     Complimenti, nessuno può nulla contro la tua fortuna (stavolta)`)
+} else if (userRoll < cpuRoll) {
+    alert(`Tu:${userRoll} vs CPU:${cpuRoll}     Impegnati di più la prossima volta`)    
+} else {
+    alert(`${userRoll}-${cpuRoll} Pari     Quasi, ma non abbastanza`)
+}
