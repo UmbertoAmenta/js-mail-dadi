6058188fd7b301c5bf265102f4b2b7a38f44522c
Umberto Amenta <umberto.amenta@hotmail.it>
2024-12-03 16:53:13 +0100

    feat: mail

--- 1-mail/script.js
+++ 1-mail/script.js
@@ -8,3 +8,41 @@ NON POTETE UTILIZZARE NESSUN METODO COME:
 - find
 - search
 - ecc...*/
+
+// |Creare| lista utenti
+
+const mailList = [
+    "mario.rossi@casual.it",
+    "john.doe@casual.en",
+    "jean.dupont@casual.fr",
+    "juan.perez@example.es",
+    "jane.smith@example.au"
+]
+
+
+
+// |Chiedere| indirizzo email all'utente
+
+const userMail = prompt("Inserisci il tuo indirizzo mail per verificare se abilitato al servizio")
+// console.log(userMail)
+
+
+
+// |Confrontare| la mail inserita con gli elementi della lista 
+
+let isUserMailValid = false
+for (let i = 0; i < mailList.length && mailList[i] === userMail; i++) {
+    isUserMailValid = true
+}
+
+
+
+// |Se| presente in lista
+//      |Informa| - accesso effettuato
+// |Altrimenti|
+//      |Informa| - accesso fallito
+if (isUserMailValid === true) {
+    alert("L'indirizzo inserito è abilitato all\'accesso al servizio")
+} else {
+    alert("L'indirizzo inserito non è valido, registrazione necessaria")
+}
